4c52a27129ba1008677747e164f2a8d6755544c6
JArgueta0310 <133625329+JArgueta0310@users.noreply.github.com>
2023-06-01 18:41:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function ocultar(){
 function mostrar(){
     document.getElementById("h3").style.display = "block";
     document.getElementById("p").style.display = "block";
-    document.querySelector('.mensaje').style.background = "url('imagenes/muñeco.png')"
+    document.querySelector('.mensaje').style.background = "url('imagenes/Muñeco.png')"
     document.querySelector('.mensaje').style.backgroundRepeat = "no-repeat";
 }
 
